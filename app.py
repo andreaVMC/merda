65bb508db1176ec556ef3e3bfb6a297963a9d1cb
Kevin <kevliujudi@gmail.com>
2024-06-09 14:35:23 +0200

    New pages

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("logged/home.html")
 
 @app.route("/settings")
 def settings():
-    return render_template("settings.html")
+    return render_template("logged/settings.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
