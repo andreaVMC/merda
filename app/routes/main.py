58d619dbeddf5b889bdc0cc1b61a64e58189e56e
Matteo Bini <matteo.bini2004@gmail.com>
2024-07-14 17:35:24 +0200

    Add teams in home

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from app.forms import ShitForm
-from app.models import Shit, User
+from app.models import Shit, User, UserTeam, Team
 from app import db
 
 bp = Blueprint('main', __name__)
@@ -14,6 +14,28 @@ def index():
 @login_required
 def home():
     form = ShitForm()
-    shits = db.session.query(Shit, User).join(User, Shit.userID == User.id).all()
+    shits_users = (
+        db.session.query(Shit, User)
+        .join(User, Shit.userID == User.id)
+        .all()
+    )
+
+    user_teams = (
+        db.session.query(UserTeam.userID, Team)
+        .join(Team, UserTeam.teamID == Team.id)
+        .all()
+    )
+
+    user_teams_map = {}
+    for user_id, team in user_teams:
+        if user_id not in user_teams_map:
+            user_teams_map[user_id] = []
+        user_teams_map[user_id].append(team)
+
+    shits = []
+    for shit, user in shits_users:
+        teams = user_teams_map.get(user.id, [])
+        shits.append((shit, user, teams))
     shits.reverse()
+
     return render_template('logged/home.html', form=form, user=current_user, shits=shits)
